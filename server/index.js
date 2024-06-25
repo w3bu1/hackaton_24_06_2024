44f791a805c21be2208308b16cfd385801038fe9
Niriniavo Safidy Nekena Rabeharivonjy <nrabehar@c1r11s7.42antananarivo.mg>
2024-06-25 10:07:51 +0300

    try express

--- server/index.js
+++ server/index.js
@@ -1,75 +1,49 @@
-const net = require("net");
+const express = require("express");
+const bodyParser = require("body-parser");
 const action = require("./action");
+
+const app = express();
+app.use(bodyParser.json());
 
 let clients = {};
 let clientId = 0;
 
-const server = net.createServer(async (socket) => {
+app.post("/connect", async (req, res) => {
+  if (Object.keys(clients).length >= 2) {
+    return res.status(400).send("Unable to connect, team already formed");
+  }
+
   clientId++;
   const currentClientId = clientId;
-  const clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
-  clients[currentClientId] = socket;
+  clients[currentClientId] = res;
 
-  console.log(
-    `Client connected: ID = ${currentClientId}, Address = ${clientAddress}`
-  );
+  console.log(`Client connected: ID = ${currentClientId}`);
   console.log(`Number of connected clients: ${Object.keys(clients).length}`);
-
-  if (Object.keys(clients).length >= 3) {
-    await action.sendMessage(
-      clients,
-      currentClientId,
-      "Unable to connect, team already formed\n"
-    );
-    await action.sendMessage(clients, currentClientId, "exit");
-    // await sendMessageToClient(currentClientId, "exit");
-    delete clients[currentClientId];
-    clientId--;
-    return;
-  }
 
   if (Object.keys(clients).length === 2) {
     await action.playRequest(clients);
   }
 
-  socket.on("data", async (data) => {
-    let id = currentClientId === 1 ? 2 : 1;
-    console.log(`Received from ${currentClientId}: ${data}`);
-    await action.sendMessage(clients, id, data.toString());
-    // await sendMessageToClient(id, data.toString());
-  });
-
-  socket.on("end", async () => {
-    console.log(
-      `Client disconnected: ID = ${currentClientId}, Address = ${clientAddress}`
-    );
+  req.on("close", () => {
+    console.log(`Client disconnected: ID = ${currentClientId}`);
     delete clients[currentClientId];
-    console.log(currentClientId);
-    id = currentClientId === 1 ? 2 : 1;
     clientId--;
-    console.log(id);
-    await action.sendMessage(clients, id, "exit");
-    // await sendMessageToClient(id, "exit");
-    console.log(`Number of connected clients: ${Object.keys(clients).length}`);
-  });
-
-  socket.on("error", (err) => {
-    console.error(`Socket error from ${currentClientId}: ${err}`);
-    delete clients[currentClientId];
     console.log(`Number of connected clients: ${Object.keys(clients).length}`);
   });
 });
 
-// // Function to send a message to a specific client by ID
-// async function sendMessageToClient(clientId, message) {
-//   if (clients[clientId]) {
-//     console.log(`Sending message to ${clientId}: ${message}`);
-//     clients[clientId].write(message);
-//   } else {
-//     console.log(`Client with ID ${clientId} does not exist`);
-//   }
-// }
+app.post("/message", async (req, res) => {
+  const { id, message } = req.body;
+  const client = clients[id];
+  if (client) {
+    await action.sendMessage(clients, id, message);
+    res.send("Message sent");
+  } else {
+    res.status(404).send("Client not found");
+  }
+});
 
-server.listen(8080, "0.0.0.0", () => {
-  console.log("TCP server is listening on port 8080");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
